f9bf34d60acd1af3700747d68ceee48f09847254
Will Chiong <will@infomofo.com>
2015-07-03 11:51:39 -0400

    initial yeoman scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mpqPartyPlannerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mpqPartyPlannerApp
+ */
+angular.module('mpqPartyPlannerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mpqPartyPlannerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mpqPartyPlannerApp
+ */
+angular.module('mpqPartyPlannerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
