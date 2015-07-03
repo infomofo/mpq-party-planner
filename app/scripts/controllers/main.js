f625de3910c995fdf510c2290531fd2d03000725
Will Chiong <will@infomofo.com>
2015-07-03 19:41:17 -0400

    implement selection- don't allow more than 3 covers to be selected at once

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,10 +11,27 @@ angular.module('mpqPartyPlannerApp')
   .controller('MainCtrl', function ($scope, $http) {
 
     $scope.mpqModel = {
-      characters: []
+      characters: [],
+      selectedCharacters: []
     };
 
-    $http.get('data/characters.json').success(function(data) {
+    $http.get('data/characters.json').success(function (data) {
       $scope.mpqModel.characters = data;
     });
+
+    $scope.select = function (character) {
+      console.log(angular.toJson(character));
+      var idx = $scope.mpqModel.selectedCharacters.indexOf(character);
+      if (idx > -1) $scope.mpqModel.selectedCharacters.splice(idx, 1);
+      else {
+        if ($scope.mpqModel.selectedCharacters.length < 3) {
+          $scope.mpqModel.selectedCharacters.push(character);
+        }
+      }
+    };
+
+    $scope.isSelectedCharacter = function (character) {
+      return $scope.mpqModel.selectedCharacters.indexOf(character) > -1;
+    };
+
   });
